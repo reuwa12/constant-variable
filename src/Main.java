8c8bdef9948b75b9ecf7225f711cb1ddb6acb191
USER <mon001024@naver.com>
2025-06-02 11:24:07 +0900

    first commit 2025-06-02

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,10 @@ public class Main {
     public static void main(String[] args) {
         int a = 10, b = 3;
 
-        a += 5; // a = a  + 5;
-
+        a++;
         System.out.println(a);
 
-        b -= 1;
-        System.out.println(b); // 2
-
-        b *= 4;
-        System.out.println(b); // 8
-
-        b /= 3;
-        System.out.println(b); // 2
-
-        b %= 2;
-        System.out.println(b); // 0
+        System.out.println(b--);
+        System.out.println(b);
     }
 }
