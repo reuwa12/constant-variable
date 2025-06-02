3d52505788d6a65654751aaa153c3e37f82a6b25
USER <mon001024@naver.com>
2025-06-02 14:48:36 +0900

    first commit 2025-06-02

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // "happy". "angry", "sad", etc.
-        String mood = "happy";
+        String mood = "tired";
 
         switch (mood) {
             case "happy":
@@ -13,6 +13,8 @@ public class Main {
             case "sad":
                 System.out.println("나는 슬프다. 왜냐하면 슬프기 때문이다.");
                 break;
+                default:
+                    System.out.println("지금 내 기분은 이 단어들로는 표현할 수 없다.");
         }
     }
 }
