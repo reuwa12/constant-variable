448086127e7901122df961330cf9398f4e247317
USER <mon001024@naver.com>
2025-06-02 15:10:59 +0900

    first commit 2025-06-02

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         // "happy". "angry", "sad", etc.
-        String mood = "tired";
+        String mood = "happy";
 
-        switch (mood) {
-            case "happy":
-                System.out.println("나는 행복하다. 왜냐하면 행복하기 때문이다.");
-                break;
-            case "angry":
-                System.out.println("나는 화가난다. 왜냐하면 화가나기 때문이다.");
-                break;
-            case "sad":
-                System.out.println("나는 슬프다. 왜냐하면 슬프기 때문이다.");
-                break;
-                default:
-                    System.out.println("지금 내 기분은 이 단어들로는 표현할 수 없다.");
-        }
+       if(mood == "happy"){
+            System.out.println("나는 행복하다. 왜냐하면 행복하기 때문이다.");
+       }else if(mood == "angry"){
+           System.out.println("나는 화가난다. 왜냐하면 화가나기 때문이다.");
+       }else if(mood == "sad"){
+           System.out.println("<나는 슬프다. 왜냐하면 슬프기 때문이다.");
+       }else{
+           System.out.println("지금 내 기분을 단어로 표현할 수 없다.");
+       }
     }
 }
